e5a5f8ebee96227269a23c6a956281c2521d0df4
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-10 19:25:45 +0000

    Implement Rank class with tests

---
+++ spec/card_spec.rb
@@ -0,0 +1,44 @@
+class Rank
+  def initialize(rank)
+    @rank = rank
+  end
+
+  def score
+   return 4 if rank == "A"
+   return 3 if rank == "K"
+   return 2 if rank == "Q"
+   1
+  end
+
+  private
+
+  attr_reader :rank
+end
+
+describe Rank do
+
+  describe '#score' do
+
+    let(:subject) { described_class.new(rank).score }
+
+    context 'when rank is Ace' do
+      let(:rank) {"A"}
+      it { is_expected.to be 4 }
+    end
+
+    context 'when rank is King' do
+      let(:rank) {"K"}
+      it { is_expected.to be 3 }
+    end
+
+    context 'when rank is Queen' do
+      let(:rank) {"Q"}
+      it { is_expected.to be 2 }
+    end
+
+    context 'when rank is Jack' do
+      let(:rank) {"J"}
+      it { is_expected.to be 1 }
+    end
+  end
+end 
